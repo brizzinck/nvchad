eb11c46b3aa365d29311738c58d89152f95891a5
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-06-13 12:58:49 +0300

    feat(augmnet): enable without suggestions

--- lua/plugins/augment.lua
+++ lua/plugins/augment.lua
@@ -1,7 +1,8 @@
 return {
-  -- "augmentcode/augment.vim",
-  -- config = function()
-  --   vim.g.augment_workspace_folders = { vim.fn.getcwd() }
-  -- end,
-  -- event = "VeryLazy",
+  "augmentcode/augment.vim",
+  config = function()
+    vim.g.augment_workspace_folders = { vim.fn.getcwd() }
+    vim.g.augment_disable_completions = true
+  end,
+  event = "VeryLazy",
 }
