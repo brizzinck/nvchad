c878aa566016f1b0db6829812893948c4b93f9f6
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-06-23 20:31:38 +0300

    fix(lazygit): handle invalid close window

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -4,4 +4,19 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  config = function()
+    local lazygit = require "lazygit"
+
+    local original_close_window = lazygit.close_window
+
+    lazygit.close_window = function(...)
+      local win_id = lazygit.win_id
+
+      if win_id and vim.api.nvim_win_is_valid(win_id) then
+        vim.api.nvim_win_close(win_id, true)
+      end
+
+      original_close_window(...)
+    end
+  end,
 }
