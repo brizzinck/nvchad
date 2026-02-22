dc5b19a65d63c3cace3d07c5532aa516e7cb3b2d
Vlad Lavrishko <lavrishkovlad@gmail.com>
2026-02-22 21:06:51 +0200

    fix(treesitter): compiling

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  lazy = false,
   build = ":TSUpdate",
-  config = function ()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup()
-    treesitter.install { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml', "rust", "go", "c++", "python" }
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml' },
-      callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-
-  end
- }
+  event = "VeryLazy",
+  opts = {
+    ensure_installed = { "lua", "python", "go", "rust", "python", "c++", "c", "yaml", "html", "css", "javascript", "typescripta" },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+}
