fd239183cb4c9e816459bb455d22a49a040908f0
Vlad Lavrishko <lavrishkovlad@gmail.com>
2026-02-23 11:48:39 +0200

    fix(treesitter): correct installing

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,58 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  event = "VeryLazy",
-  opts = {
-    ensure_installed = { "lua", "python", "go", "rust", "python", "c++", "c", "yaml", "html", "css", "javascript", "typescripta" },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  config = function()
+    require("nvim-treesitter").setup {
+      ensure_installed = {
+        "bash",
+        "comment",
+        "css",
+        "diff",
+        "dockerfile",
+        "elixir",
+        "git_config",
+        "gitcommit",
+        "gitignore",
+        "groovy",
+        "go",
+        "heex",
+        "hcl",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rst",
+        "rust",
+        "scss",
+        "ssh_config",
+        "sql",
+        "terraform",
+        "typst",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+
+    vim.treesitter.language.register("groovy", "Jenkinsfile")
+
+    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.wo[0][0].foldmethod = "expr"
+  end,
 }
