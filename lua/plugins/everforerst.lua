533ad9a8fff527e18bc47a54d74f8ddf9fc8deac
Vlad Lavrishko <lavrishkovlad@gmail.com>
2026-03-07 01:33:53 +0200

    fix(everforest): background disable

--- lua/plugins/everforerst.lua
+++ lua/plugins/everforerst.lua
@@ -1,19 +1,17 @@
--- in your plugins/init.lua or a dedicated theme file
 return {
   "neanias/everforest-nvim",
-  version = false, -- or specify a version like 'v2.0'
+  version = false,
   lazy = true,
-  priority = 1000, -- Ensures it loads early
+  priority = 1000,
   config = function()
     require("everforest").setup {
-      -- Your configuration options here
-      background = "hard", -- Options: 'soft', 'medium', 'hard'
-      transparent_background_level = 0, -- 0-2, higher means more transparent UI
-      italics = false, -- Whether to use italics for keywords
-      disable_italic_comments = false, -- Set to true to disable italics for comments
-      sign_column_background = "none", -- Options: 'none', 'grey'
-      ui_contrast = "low", -- Options: 'high', 'low'
+      background = "hard",
+      transparent_background_level = 2,
+      italics = false,
+      disable_italic_comments = false,
+      sign_column_background = "none",
+      ui_contrast = "low",
     }
-    require("everforest").load() -- Load the color scheme
+    require("everforest").load()
   end,
 }
